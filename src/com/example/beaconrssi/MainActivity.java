bcef83998a4d38fbbb039ed7694070a0e014ccb3
CLINK-ueda <ueda@communitylink.jp>
2014-09-23 17:36:53 +0900

    加速度・ジャイロセンサで測位する

--- src/com/example/beaconrssi/MainActivity.java
+++ src/com/example/beaconrssi/MainActivity.java
@@ -8,12 +8,18 @@ import android.bluetooth.BluetoothManager;
 import android.content.Intent;
 //import android.graphics.Color;
 //import android.net.Uri;
+
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.os.Handler;
 import android.text.format.Time;
 import android.text.method.ScrollingMovementMethod;
 import android.view.View;
 import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -22,26 +28,50 @@ import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 
-import java.util.ArrayList;
-import java.util.TimerTask;
+import java.math.BigDecimal;
 import java.util.*;
 
-// import android.util.Log;
+
+import android.util.Log;
 
 // マニフェストファイル(～Manifest.xml)にGPSのパーミッションを追加すること！！
-public class MainActivity extends Activity implements LocationListener {
-	private LocationManager manager = null; // GPS測位に用いる
-	// private Musictest view;
+public class MainActivity extends Activity implements LocationListener,SensorEventListener {
+    private SensorManager sensorManager;		//センサーマネージャ
+    private Sensor        accelerometer;		//加速度センサー
+    private Sensor        orientation;  		//回転センサー
+    private double[] senvalues=new double[6];	//加速度と傾き値
+	private LocationManager manager = null; 	// GPS測位に用いる
+
+	double lowPassX,lowPassY,lowPassZ;		// 加速度を加重平均のため記録する
+	double rawAx,rawAy,rawAz;				// ハイパスフィルタを通った値
+    
+	private static double azimuth ,pitch , roll;	// 方位、ピッチ、ロール
+	private static double ax,ay,az;			// 各方向加速度
+	private static int vx,vy,vz;				// 各方向速度
+	private static int dvx,dvy,dvz;			// 各方向微小速度
+	private static int x,y,z;					// 各方向移動距離
+	private static int dx,dy,dz;				// 各方向微小移動距離
+	private static long nowTime;
+	private static long oldTime;				// 前回測定時刻
+
+	static double offAx = +0.0;		// オフセット
+	static double offAy = -0.0;		// オフセット
+	static double offAz = +0.0;		// オフセット
+	static double k = 0.1;	// 加重平均の係数
+	static int snflag = 0;	// センサ初期化フラグ
+    
+	// Bluetooth関係の変数
     private static final int REQUEST_ENABLE_BLUETOOTH = 1;
 	static BluetoothAdapter mBluetoothAdapter;
     private TextView mResultView;
+    
     // ライブラリを使用しないBluetooth関係の変数
 	int BCONmajor;
 	int BCONminor;
 	String BCONuuid;
 	    
     //監視対象となるビーコンの値
-    private static final String TARGET_UUID = "ここにUUIDを記載";
+    private static final String TARGET_UUID = "00000000-EA00-1001-B000-001C4D25E26A";
     // ビーコンの２つのIDの最大値
     private static final int TARGET_MAJOR = 1;
     private static final int TARGET_MINOR = 5;
@@ -69,6 +99,8 @@ public class MainActivity extends Activity implements LocationListener {
     
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+    	// スリープしない
+    	getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);  
     	// 変数などを設定する
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
@@ -99,6 +131,16 @@ public class MainActivity extends Activity implements LocationListener {
         // GPSサービス取得
         manager = (LocationManager)getSystemService(LOCATION_SERVICE);
 // 測位関係の初期化 ここまで ***********************************************************
+
+        //センサーマネージャの取得(1)
+        sensorManager=(SensorManager)getSystemService(
+            Context.SENSOR_SERVICE);
+        //センサーの取得(2)
+        List<Sensor> list;
+        list=sensorManager.getSensorList(Sensor.TYPE_ACCELEROMETER);
+        if (list.size()>0) accelerometer=list.get(0);
+        list=sensorManager.getSensorList(Sensor.TYPE_ORIENTATION);
+        if (list.size()>0) orientation=list.get(0);
         
         // 以下表示関係-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-
         // readyテキストを表示させる[uedaデバッグ]
@@ -134,6 +176,15 @@ public class MainActivity extends Activity implements LocationListener {
                 moniteringRssi();
                 mResultView.setBackgroundColor(0x33ff0000);	// 背景色を赤系に変える
                // setContentView(mResultView);
+
+			    // フラグをセット
+			    snflag = 0;
+			    
+                azimuth = pitch = roll = 0;			// 方位、ピッチ、ロール
+            	lowPassX = lowPassY = 0;	// オフセット
+            	lowPassZ = 9.8066;
+            	rawAx = rawAy = rawAz = 0;			// 初期化
+                
             }
         });
     }
@@ -202,9 +253,23 @@ public class MainActivity extends Activity implements LocationListener {
                     Time time = new Time("Asia/Tokyo");	//時刻を取得
                     time.setToNow();					//現在の時刻を設定
                     locationArray[0] = time.year + "年" + (time.month+1) + "月" + time.monthDay + "日  "+ time.hour + "時" + time.minute + "分" + time.second + "秒";
-           		
+
+                    // システム時刻を表示する
+                    mResultView.append("システム時刻："+nowTime+"\n");
                     // 時刻、緯度、経度を表示する
-                    mResultView.append("時刻："+locationArray[0]+"\n緯度："+locationArray[1]+"  経度："+locationArray[2]+"\n\n");
+                    mResultView.append("時刻："+locationArray[0]+"\t\t緯度："+locationArray[1]+"  経度："+locationArray[2]+"\n");
+                    // 加速度と傾きを表示する
+                    mResultView.append( "X軸加速度:"+senvalues[0]+"　Y軸加速度:"+senvalues[1]+"　Z軸加速度:"+senvalues[2]+"\nQ:"+senvalues[3]+"　P:"  +senvalues[4]+"　R:"  +senvalues[5]+"\n");
+                    mResultView.append( "rawAx:"+rawAx+"\trawAy:"+rawAy+"\trawAz:"+rawAz+"\nax:"+ax+"\tay:"+ay+"\taz:"+az+"\n\n");
+
+                    // 微小速度を表示する
+                    mResultView.append("dvx:"+dvx+"mm/s\tdvY:"+dvy+"mm/s\tdvz:"+dvz+"mm/s\n");
+                    // 速度を表示する
+                    mResultView.append("vX:"+vx+"mm/s\tvY:"+vy+"mm/s\tvZ:"+vz+"mm/s\n\n");
+                    // 微小距離を表示する
+                    mResultView.append("dx:"+dx+"mm\tdY:"+dy+"mm\tdz:"+dz+"mm\n");
+                    // 移動距離を表示する
+                    mResultView.append("X:"+(int)x+"cm\tY:"+(int)y+"cm\tz:"+(int)z+"cm\n\n");
            	//今回追加した部分　ここまで***********************************************************
 
                     // UUIDを表示
@@ -315,6 +380,104 @@ public class MainActivity extends Activity implements LocationListener {
 		    		}
 		    	}
 		    }
+
+//　センサ　ここから***********************************************************		
+
+			@Override
+		    //センサーリスナーの処理(5)
+		    public void onSensorChanged(SensorEvent event) {
+				int muda;
+		        //加速度の取得0
+		        if (event.sensor==accelerometer) {
+		            senvalues[0]=event.values[0];	// 取得した値を配列に保存
+		            senvalues[1]=event.values[1];
+		            senvalues[2]=event.values[2];
+				    // ***********************************************************
+					// Low Pass Filter
+					lowPassX += (senvalues[0] - lowPassX) * k;
+					lowPassY += (senvalues[1] - lowPassY) * k;
+					lowPassZ += (senvalues[2] - lowPassZ) * k;
+	
+					// High Pass Filter
+					rawAx = senvalues[0] - lowPassX;
+					rawAy = senvalues[1] - lowPassY;
+					rawAz = senvalues[2] - lowPassZ;
+					//オフセット
+					rawAx -= offAx;
+					rawAy -= offAy;
+					rawAz -= offAz;
+					Log.d("Count","加速度");
+		        }
+		        //方向の取得
+		        if (event.sensor==orientation) {
+		            senvalues[3]=event.values[0];	// 取得した値を配列に保存
+		            senvalues[4]=event.values[1];
+		            senvalues[5]=event.values[2];
+		            
+				    azimuth = senvalues[3];
+				    pitch = senvalues[4];
+				    roll = senvalues[5];
+					Log.d("Count","回転");
+			// ***********************************************************
+					// ピッチ
+					double nPitchRad = Math.toRadians(-pitch); // n means negative
+					double sinNPitch = Math.sin(nPitchRad);
+					double cosNPitch = Math.cos(nPitchRad);
+					// ロール
+					double nRollRad = Math.toRadians(-roll);
+					double sinNRoll = Math.sin(nRollRad);
+					double cosNRoll = Math.cos(nRollRad);
+					// 方位（アジマス）
+					double nAzimuthRad = Math.toRadians(-azimuth);
+					double sinNAzimuth = Math.sin(nAzimuthRad);
+					double cosNAzimuth = Math.cos(nAzimuthRad);
+					// 一時退避変数
+					double bx, by; 
+					bx = rawAx * cosNRoll + rawAz * sinNRoll;
+					by = rawAx * sinNPitch * sinNRoll + rawAy * cosNPitch - rawAz * sinNPitch * cosNRoll;
+					// 端末内部の座標から実空間での座標へ変換
+					az = (-rawAx * cosNPitch * sinNRoll + rawAy * sinNPitch * cosNRoll + rawAz * cosNPitch * cosNRoll)*1000;
+					ax = (bx * cosNAzimuth - by * sinNAzimuth)*1000;
+					ay = (bx * sinNAzimuth + by * cosNAzimuth)*1000;
+		        }
+		        Log.d("Sensor", "X軸加速度:"+senvalues[0]+"　Y軸加速度:"+senvalues[1]+"　Z軸加速度:"+senvalues[2]+"\n" +
+		        		"方位:"+senvalues[3]+"　ピッチ:"  +senvalues[4]+"　ロール:"  +senvalues[5]);
+
+			   // ax, ay, az が求まった後で
+			   nowTime = System.currentTimeMillis();	//システムの現在時刻をミリ秒（long型の数値）で取得
+			   long interval = nowTime - oldTime; // intervalをミリ秒から秒へ変換
+			   oldTime = nowTime;			
+	           if(snflag == 1){
+	        	   // ***********************************************************
+	        	   // 加速度を積分する
+	        	   dvx = (int) (ax * interval/1000); // 速度[mm/s] にする
+	        	   dvy = (int) (ay * interval/1000);
+	        	   dvz = (int) (az * interval/1000);
+	        	   vx += dvx;
+	        	   vy += dvy;
+	        	   vz += dvz;
+	        	   
+	        	   
+	        	   dx = (int) (vx * interval/1000);	// 距離[mm] にする
+	        	   dy = (int) (vy * interval/1000);
+	        	   dz = (int) (vz * interval/1000);
+	        	   x += dx/10;						// 距離[cm] にする
+	        	   y += dy/10;
+	        	   z += dz/10;
+	        	   // ***********************************************************
+	            }else{
+	            	vx = vy = vz = 0;					//　速度初期化
+	            	x = y = z = 0 ;						//　移動距離初期化
+	            	// フラグをセット
+				    snflag = 1;
+	            }
+			}
+
+		    //精度変更イベントの処理
+		    public void onAccuracyChanged(Sensor sensor,int accuracy) {
+		    }
+
+//　センサ　ここまで***********************************************************	
 // 測位　ここから ***********************************************************
 		    @Override
 		    protected void onPause() {
@@ -335,6 +498,17 @@ public class MainActivity extends Activity implements LocationListener {
 		            		0, 0, this);
 		        }
 		        super.onResume();	//これを省くとアプリが落ちる
+		        
+		        //センサーの処理の開始(3)
+		        if (accelerometer!=null) {
+		            sensorManager.registerListener(this,accelerometer,
+		                SensorManager.SENSOR_DELAY_GAME);
+		        }
+		        if (orientation!=null) {
+		            sensorManager.registerListener(this,orientation,
+		                SensorManager.SENSOR_DELAY_GAME);
+		        }
+		        
 		    }
 		    // 測位が実行されると呼ばれる
 		    @Override
@@ -359,7 +533,15 @@ public class MainActivity extends Activity implements LocationListener {
 		    public void onStatusChanged(String provider, int status, Bundle extras) {
 		        // TODO Auto-generated method stub
 		    }
-//　測位　ここまで***********************************************************		    
+
+		    @Override
+		    protected void onStop() {
+		      super.onStop();
+		      //センサーの処理の停止
+		      sensorManager.unregisterListener(this);
+		    }
+
+//　測位　ここまで***********************************************************	
 // Logをトーストで表示する処理***********************************************************    
 // 表示に関する処理=====================================================================
 //ログをトーストで表示**********************************************************
@@ -378,6 +560,7 @@ public class MainActivity extends Activity implements LocationListener {
         AlertDialog dialog = builder.create();
         dialog.show();
     }
+
 }
 /* 測位のもとはここ↓
  * http://seesaawiki.jp/w/moonlight_aska/d/%B0%CC%C3%D6%BE%F0%CA%F3%A4%F2%BC%E8%C6%C0%A4%B9%A4%EB
